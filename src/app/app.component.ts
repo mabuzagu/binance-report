2c04446f4e890210e4a49f0bd5675510e01709a1
Gugu Mabuza <mabuzagu@gmail.com>
2018-03-19 00:44:02 -0400

    Save input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,8 @@ import { Component } from '@angular/core';
 import { ElectronService } from 'ngx-electron';
 import { BinanceService } from './binance.service';
 
+
 import { IPrice } from './price.interface';
-
-const binance = require('node-binance-api');
-
-binance.options({
-  APIKEY: 'BtVEjw9f4QX7ujc5RFg2Actywwyp3qXwMRBQru8X2hnFFGVvQ4ytV7oDlGNnP8yA',
-  APISECRET: 'K5dO6QusJfFWHPDRDm9IzWkYMBpFDvStSMpZYlESdYHcztLxQNfRvefGr2SEelyi',
-  useServerTime: true
-});
 
 @Component({
   selector: 'app-root',
@@ -21,36 +14,47 @@ binance.options({
 export class AppComponent {
 
   prices: IPrice[] = [];
+  balances: any = [];
+  keys: any = [];
   errorMessage: string;
+  accountKeys: any;
 
   title = 'BinanceReport';
 
-  constructor(private _electronService: ElectronService,
-    private _binanceService: BinanceService
+  constructor( private _binanceService: BinanceService
   ){
 
   }
 
-
-
-  launchWindow() {
-    this._electronService.shell.openExternal('https://binance.com');
-  }
-
   ngOnInit(): void {
 
+  }
 
+  saveKeys() : void {
 
-    binance.balance((error, balances) => {
-      console.log("balances()", balances);
-      console.log("ETH balance: ", balances.ETH.available);
-    });
+  }
 
-    this._binanceService.getPrices()
-                  .subscribe(
-                    prices => this.prices = prices,
-                    error => this.errorMessage = <any>error
-                  )
+  getBalance(): void {
+
+    // const binance = require('node-binance-api');
+    //
+    // binance.options({
+    //   APIKEY: '',
+    //   APISECRET: '',
+    //   useServerTime: true
+    // });
+    //
+    // binance.balance((error, balances) => {
+    //   console.log("balances()", balances);
+    //   console.log("ETH balance: ", balances.ETH.available);
+    // });
+
+    // this._binanceService.getPrices()
+    //               .subscribe(
+    //                 prices => this.prices = prices,
+    //                 error => this.errorMessage = <any>error
+    //               )
+
   }
 
 
